847672cd7b801254db098d8e59200234cb67affa
ashpooljh <ashpool@xecut.net>
2020-01-28 23:42:34 +0300

    Basic algo to infer weights from given bounds

--- README.md
+++ README.md
@@ -11,15 +11,23 @@ Python 3.5+
 $ python -i dumbbell.py
 >>> d = Dumbbell(grip=1.5, disks=[0.75, 1, 2.5, 5])
 ```
-`grip` represents the weight of the dumbbell's grip.  
+This creates a dumbbell object with fixed grip weight and set of disk weights.\
+`grip` represents the weight of the dumbbell's grip.\
 `disks` is a list of disk weights, it's assumed that 2 disks are available of each of them.
+```
+>>> d = Dumbbell.calc_exact(min_weight=1.5, max_weight=20, max_delta=1.5) 
+```
+This creates a dumbbell with automatically inferred grip and disk weights.\
+`min_weight` represents the minimum possible weight (no disks attached, only grip)\
+`max_weight` represents the maximum possible weight (all disks attached)\
+`max_delta` stands for maximum step allowed between adjacent configurations
 ```
 >>> d.configurations()
 [1.5, 3.0, 3.5, 5.0, 6.5, 8.0, 8.5, 10.0, 11.5, 13.0, 13.5, 15.0, 16.5, 18.0, 18.5, 20.0]
 ```
 This returns a sorted list of all possible weight configurations for this dumbbell.
 ```
->>> d.maxdelta()
+>>> d.max_delta()
 1.5
 ```
 This returns a maximum weight difference between any two configurations adjacent in the list.
--- dumbbell.py
+++ dumbbell.py
@@ -1,4 +1,4 @@
-from itertools import combinations
+from itertools import combinations, takewhile
 
 class Dumbbell:
     def __init__(self, grip, disks):
@@ -12,7 +12,25 @@ class Dumbbell:
                 weights.add(self.grip + 2 * sum(conf))
         return sorted(weights)
 
-    def maxdelta(self):
+    def max_delta(self):
         confs = self.configurations()
-        return max((confs[i] - confs[i - 1] \
+        return max((confs[i] - confs[i - 1]
                for i in range(1, len(confs))))
+
+    @classmethod
+    def calc_exact(cls, min_weight, max_weight, max_delta):
+        weight_range = max_weight - min_weight
+        weights = []
+        if weight_range <= 0:
+            return cls(grip=min_weight, disks=[])
+        total_disks_weight = 0
+        power_of_2 = 0
+        while total_disks_weight < weight_range:
+            new_weight = (2 ** (power_of_2 - 1)) * max_delta
+            weights.append(new_weight)
+            total_disks_weight += new_weight * 2
+            power_of_2 += 1
+        overhead = total_disks_weight - weight_range
+        if overhead >= 1:
+            weights[-1] -= (overhead / 2)
+        return cls(grip=min_weight, disks=weights)
